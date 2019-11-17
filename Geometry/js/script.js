38d43c06f3288ec52c9bc65e1fe48e9348bdda0e
Artem Gusev <gusev2014russia@mail.ru>
2019-11-17 19:32:55 +0300

    code refactoring

--- Geometry/js/script.js
+++ Geometry/js/script.js
@@ -1,35 +1,55 @@
-let scene= new THREE.Scene(),
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000),
-    renderer = new THREE.WebGLRenderer({antialias: true});
+import "./libs/three.min.js";
 
-renderer.setClearColor("#e5e5e5");
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+class Torus{
+    constructor(){
+        this.scene= new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+        this.renderer = new THREE.WebGLRenderer({antialias: true});
+        this.geometry =  new THREE.TorusGeometry(2, .2, 10, 100 );
+        this.material = new THREE.MeshPhongMaterial( { color: 0x1aacd0 } );
+        this.mesh = new THREE.Mesh( this.geometry, this.material );
+        this.light=new THREE.DirectionalLight(0xffffff,1.2);
 
-let geometry =  new THREE.TorusGeometry(2, .2, 10, 100 ),
-    material = new THREE.MeshPhongMaterial( { color: 0x1aacd0 } ),
-    mesh = new THREE.Mesh( geometry, material );
-mesh.position.set(0,0,-7);
-scene.add(mesh);
+    }
 
-camera.position.z =10;
-camera.position.y=6;
-camera.position.x=2;
+    settings(){
+        this.renderer.setClearColor("#e5e5e5");
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
 
-let light=new THREE.DirectionalLight(0xffffff,1.2);
-light.position.set(3,3,4);
-scene.add(light);
+        this.mesh.position.set(0,0,-7);
+        this.scene.add(this.mesh);
 
-renderer.render(scene,camera);
+        this.light.position.set(3,3,4);
+        this.scene.add(this.light);
+        this.camera.position.set(0,3,5);
 
-window.addEventListener('resize', ()=>{
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect=window.innerWidth/window.innerHeight;
-    camera.updateProjectionMatrix();
-});
-let render=function (){
-    requestAnimationFrame(render);
-    mesh.rotation.y+=0.05;
-    renderer.render(scene, camera);
-};
-render();
+        this.renderer.render(this.scene,this.camera);
+
+        this.resizePage();
+    }
+
+    resizePage(){
+        window.addEventListener('resize', ()=>{
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+            this.camera.aspect=window.innerWidth/window.innerHeight;
+            this.camera.updateProjectionMatrix();
+        });
+    }
+
+    render(){
+        requestAnimationFrame(this.render.bind(this));
+        this.mesh.rotation.y+=0.05;
+        this.renderer.render(this.scene, this.camera);
+    };
+}
+
+const Hoop = new Torus();
+Hoop.settings();
+Hoop.render();
+
+
+
+
+
+
--- Man/js/script.js
+++ Man/js/script.js
@@ -1,30 +1,52 @@
-const scene=new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000),
-    renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setClearColor("#e5e5e5");
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-camera.position.set(0,100,300);
-let light = new THREE.AmbientLight(0xffffff, 5);
-scene.add(light);
-let loader = new THREE.GLTFLoader();
-loader.load('scene.gltf', meshLoad);
-let mesh;
-render();
-function meshLoad(gltf) {
-    mesh = gltf.scene;
-    console.log(mesh.children[0]);
-    mesh.children[0].material = new THREE.MeshLambertMaterial();
-    scene.add(mesh);
-    mesh.scale.set(.5,.5,.5);
+import "./libs/three.min.js";
+import "./libs/GLTFLoader.js";
+
+class Man{
+    constructor(){
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+        this.renderer = new THREE.WebGLRenderer({antialias: true});
+        this.light = new THREE.AmbientLight(0xffffff, 5);
+        this.objLoader = new THREE.GLTFLoader();
+    }
+
+    settings(){
+        this.renderer.setClearColor("#e5e5e5");
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+        this.camera.position.set(0,100,300);
+        this.scene.add(this.light);
+
+        this.resizeWindow();
+        this.objLoader.load('scene.gltf', this.meshLoad.bind(this));
+    }
+
+    meshLoad(gltf) {
+        this.mesh = gltf.scene;
+        this.mesh.children[0].material = new THREE.MeshLambertMaterial();
+        this.scene.add(this.mesh);
+        this.mesh.scale.set(.5,.5,.5);
+    }
+
+    resizeWindow(){
+        window.addEventListener('resize', ()=>{
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+            this.camera.aspect = window.innerWidth/window.innerHeight;
+            this.camera.updateProjectionMatrix();
+        });
+    }
+
+    render() {
+        if (this.mesh) this.mesh.rotation.y += 0.01;
+        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(this.render.bind(this));
+    }
 }
-function render() {
-    if (mesh) mesh.rotation.y += 0.01;
-    renderer.render(scene, camera);
-    requestAnimationFrame(render);
-}
-window.addEventListener('resize', ()=>{
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect=window.innerWidth/window.innerHeight;
-    camera.updateProjectionMatrix();
-});
+
+const Person = new Man();
+Person.settings();
+Person.render();
+
+
+
+
