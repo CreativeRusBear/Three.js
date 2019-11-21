564fe6a139a467ec3b1188f40ed3d434ed2f7abf
Artem Gusev <gusev2014russia@mail.ru>
2019-11-21 20:42:03 +0300

    code refactoring

--- Banana/js/main.js
+++ Banana/js/main.js
@@ -1,49 +1,88 @@
-const scene = new THREE.Scene(),
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000),
-    renderer = new THREE.WebGLRenderer({antialias:true, alpha: true});
+import "./libs/three.min.js";
+import "./libs/OrbitControls.js";
+import "./libs/OBJLoader.js";
+import "./libs/MTLLoader.js";
 
-renderer.setClearColor('#dddddd');
+class Banana {
+    constructor() {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
 
-renderer.setSize(window.innerWidth,window.innerHeight);
-document.body.appendChild(renderer.domElement);
+        this.keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1);
+        this.fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(7,100%,68%)'), 0.75);
+        this.backLight = new THREE.DirectionalLight(0xffffff, 1);
 
-camera.position.set(5,2,1);
-let keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1);
-keyLight.position.set(-100, 0, 100);
-let fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(7,100%,68%)'), 0.75);
-fillLight.position.set(100, 0, 100);
-let backLight = new THREE.DirectionalLight(0xffffff, 1);
-backLight.position.set(100, 0, -100).normalize();
-scene.add(keyLight);
-scene.add(fillLight);
-scene.add(backLight);
+        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
 
+        window.addEventListener('resize', this.windowResize.bind(this));
+    }
 
-let textureObjLoader = new THREE.TextureLoader(),
-    map = textureObjLoader.load('./model/banana_texture.png'),
-    material = new THREE.MeshPhongMaterial({map: map}),
-    objLoader = new THREE.OBJLoader();
-objLoader.load( './model/banana.obj', function ( obj ) {
-    obj.traverse( function ( node ) {
-        if ( node.isMesh ) node.material = material;
-    } );
-    scene.add( obj );
-} );
+    settings() {
+        this.lightsSettings();
+        this.sceneSettings();
+        this.camera.position.set(5, 2, 1);
 
-let controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.enableDamping=true;
-controls.campingFactor=0.25;
-controls.enableZoom=true;
+    }
 
-function render() {
-    renderer.render(scene,camera);
-    controls.update();
-    requestAnimationFrame(render);
+    lightsSettings() {
+        this.lightsPositions();
+        this.addLightsOnScene();
+    }
+
+    lightsPositions() {
+        this.keyLight.position.set(-100, 0, 100);
+        this.fillLight.position.set(100, 0, 100);
+        this.backLight.position.set(100, 0, -100).normalize();
+    }
+
+    addLightsOnScene() {
+        this.scene.add(this.keyLight);
+        this.scene.add(this.fillLight);
+        this.scene.add(this.backLight);
+    }
+
+    sceneSettings() {
+        this.renderer.setClearColor('#dddddd');
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+        this.controlsSettings();
+    }
+
+    controlsSettings() {
+        this.controls.enableDamping = true;
+        this.controls.campingFactor = 0.25;
+        this.controls.enableZoom = true;
+    }
+
+    windowResize() {
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+    }
+
+    loadObj() {
+        const textureObjLoader = new THREE.TextureLoader();
+        const map = textureObjLoader.load('./model/banana_texture.png');
+        const material = new THREE.MeshPhongMaterial({map: map});
+        const objLoader = new THREE.OBJLoader();
+        objLoader.load('./model/banana.obj', obj => {
+            obj.traverse(node => {
+                if (node.isMesh) node.material = material;
+            });
+            this.scene.add(obj);
+        });
+    }
+
+    render() {
+        this.renderer.render(this.scene, this.camera);
+        this.controls.update();
+        requestAnimationFrame(this.render.bind(this));
+    }
 }
-render();
 
-window.addEventListener('resize', ()=>{
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect=window.innerWidth/window.innerHeight;
-    camera.updateProjectionMatrix();
-});
+const banana = new Banana();
+banana.settings();
+banana.loadObj();
+banana.render();
+
